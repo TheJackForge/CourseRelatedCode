bf3faaea9219a53b65219ac1c6237e8e20f5fb37
TheJackForge <kimfidler@gmail.com>
2020-11-14 09:59:07 -0700

    Added Some New Sort Functions

--- DOM Array Methods/app2.js
+++ DOM Array Methods/app2.js
@@ -1,12 +1,15 @@
 const addUser = document.getElementById('add-user');
 const double = document.getElementById('double');
 const showMillion = document.getElementById('show-millionaires');
-const sort = document.getElementById('sort');
+const sortPoor = document.getElementById('sort-poor')
+const sortRich = document.getElementById('sort-rich');
 const wealth = document.getElementById('calculate-wealth')
 const main = document.getElementById('main');
+const sortAlphabet = document.getElementById('sort-alphabet')
 
 let data = [];
 
+// Pull a random person from API
 
 async function getRandomPerson() {
     const response = await fetch('https://randomuser.me/api/') 
@@ -20,11 +23,14 @@ async function getRandomPerson() {
 addData(newUser)
 }
 
+// Add a random person to the data Array variable
 
 function addData(user) {
     data.push(user);
     updateDom()
 }
+
+// Add's the random person the DOM and repopulates the main div
 
 function updateDom(providedData = data) {
         main.innerHTML = `<h2><strong>Person</strong> Wealth</h2>`;
@@ -36,9 +42,13 @@ function updateDom(providedData = data) {
         })
     }
 
+// Converts a number to a dollar amount
+
 function convertMoney(number) {
     return '$' + number.toFixed(2).replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, "$1,")
 }
+
+// Doubles one of the items money
 
 function doubleMoney() {
     data = data.map((user) => {
@@ -50,5 +60,56 @@ function doubleMoney() {
     updateDom()
 }
 
+// Filters the people that have over $1,000,000
+
+function filterMillion() {
+    data = data.filter(function (user) {
+        return user.money > 1000000;
+    })
+    updateDom()
+}
+
+// Sort Users in Alphabetical Order
+
+function sortAlphabetically() {
+    data = data.sort((a, b) => {
+        if (a.name > b.name) {
+            return 1;
+        } else {
+            return -1;
+        }
+    })
+    updateDom()
+}
+
+// Sort by Richest
+
+function sortRichest() {
+    data = data.sort((a, b) => {
+        if (b.money > a.money) {
+            return 1
+        } else {
+            return -1
+        }
+    })
+    updateDom()
+}
+
+function sortPoorest() {
+    data = data.sort((a, b) => {
+    if (a.money > b.money) {
+        return 1
+    } else {
+        return -1
+    }
+    })
+    updateDom()
+}
+
+
 addUser.addEventListener('click', getRandomPerson)
 double.addEventListener('click', doubleMoney);
+showMillion.addEventListener('click', filterMillion)
+sortAlphabet.addEventListener('click', sortAlphabetically)
+sortRich.addEventListener('click', sortRichest)
+sortPoor.addEventListener('click', sortPoorest)
