4648b3c7b81d8b7dd1bccd0a4c1efd54765d107b
TheJackForge <kimfidler@gmail.com>
2020-11-18 14:32:23 -0700

    Total / Income / Expense Code Added

--- Expense Tracker/app.js
+++ Expense Tracker/app.js
@@ -18,6 +18,9 @@ let transactions = dummyTransactions;
 function initApp() {
     list.innerHTML = ''
     transactions.forEach(addTransactionsDOM)
+
+    updateValue();
+   
 }
 
 function addTransactionsDOM(transaction) {
@@ -35,6 +38,21 @@ function addTransactionsDOM(transaction) {
     <button class="delete-btn">x</button>
     `
     list.appendChild(item);
+}
+
+function updateValue() {
+    const amounts = transactions.map( item => item.amount)
+
+    const total = amounts.reduce(( acc, item ) => acc += item );
+    balance.innerText = `$${(total).toFixed(2)}`
+    
+    const income = amounts.filter( item => item > 0).reduce((acc, item) => acc += item)
+    moneyPlus.innerText = `$${(income).toFixed(2)}`
+    console.log(income);
+
+    const expense = amounts.filter( item => item < 0).reduce((acc, item) => acc += item)
+    moneyMinus.innerText = `$${(expense * -1).toFixed(2)}`
+    console.log(expense);
 }
 
 initApp();
