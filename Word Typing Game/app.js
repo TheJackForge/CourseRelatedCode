d39c7eb75056e915f5d42ba72c1fa876a2efaa98
TheJackForge <kimfidler@gmail.com>
2020-11-21 11:17:15 -0700

    Score Add, Time Add, Difficulty Change

--- Word Typing Game/app.js
+++ Word Typing Game/app.js
@@ -2,31 +2,33 @@ const settingsBtn = document.getElementById('settings-btn');
 const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settings-form');
 const text = document.getElementById('text');
+const difficulty = document.getElementById('difficulty');
 
-
+console.log(difficulty.value)
 const wordArray = ['Apple', 'Orange', 'Banana', 'Tangerine', 'Lime', 'Cherry']
 
 let word = document.getElementById('word')
-let time = document.getElementById('time');
-let score = document.getElementById('score');
+let timer = document.getElementById('time')
+let score = document.getElementById('score')
 
-score.innerText = +0;
-time.innerText = 10;
+score.innerHTML = 0;
+time.innerText = +10;
 
 settingsBtn.addEventListener('click', () => {
     settings.classList.toggle('hide');
+    
 })
 
 function getNewWord() {
     word.innerText = wordArray[Math.floor(Math.random() * wordArray.length)]
     }
 
-function countdown() {
-        time.innerText--;
-    }
-
 function clearInput() {
     text.value = ''
+}
+
+function difficultySelect() {
+    console.log(difficulty.value)
 }
 
 function gamePlay() {
@@ -34,15 +36,15 @@ function gamePlay() {
     const newWord = word.innerText;
 
     if (answer === newWord) {
-        time.innerText + 4;
         score.innerText++;
+        time.innerText = +time.innerText + 5;
         clearInput();
         getNewWord();
     }
      console.log(newWord);
 }
 
-
+difficulty.addEventListener('change', difficultySelect)
 text.addEventListener('input', gamePlay)
 
 getNewWord();
