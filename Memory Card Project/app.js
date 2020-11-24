988ae53a83262639b9c0e36f9c22aa6e62b08154
TheJackForge <kimfidler@gmail.com>
2020-11-24 12:49:41 -0700

    Flipcard project complete

--- Memory Card Project/app.js
+++ Memory Card Project/app.js
@@ -8,51 +8,127 @@ const cardsContainer = document.getElementById('cards-container')
 const prevBtn = document.getElementById('prev')
 const nextBtn = document.getElementById('next')
 const clear = document.getElementById('clear')
+const current = document.getElementById('current')
 
-function createCard() {
-    const questionText = question.value
-    const answerText = answer.value
-    if (questionText === '' || answerText === '') {
-        alert('Please Fill In All Fields')
-    } else {
+
+// Keep track of current card
+let currentCard = 0;
+
+// Store the DOM cards
+
+const cardsEl = [];
+
+// Store the cards data
+
+const cardsData = getCardsData();
+
+function getCardsData() {
+    const cards = JSON.parse(localStorage.getItem('cards'))
+    return cards === null ? [] : cards ;
+}
+
+function setCardsData(cards) {
+    localStorage.setItem('cards', JSON.stringify(cards));
+    window.location.reload();
+}
+
+function createNewCard() {
+
+        const cardQuestion = question.value
+        const cardAnswer = answer.value
+
+        console.log(cardQuestion)
+        console.log(cardAnswer)
+
+    if (question && answer) {
+        const newCard = {
+            question: cardQuestion,
+            answer: cardAnswer
+        }
+        createCards(newCard);
+        question.value = ''
+        answer.value = ''
+
+        addContainer.classList.remove('show')
+
+        cardsData.push(newCard)
+        setCardsData(cardsData);
+    }
+}
+
+function createCards() {
+
+ cardsData.forEach((card, index) => createCard(card, index))
+
+}
+
+function updateCurrentText() {
+    console.log(currentCard)
+    current.innerText = `${currentCard + 1} / ${cardsEl.length}`
+}
+
+function createCard(data, index) {
+
     let card = document.createElement('div')
+    if (index === 0) {
+        card.classList.add('active')
+    }
+
     card.classList.add('card');
-    card.classList.add('active')
-    console.log(cardsContainer.innerHTML)
     card.innerHTML = `
             <div class="inner-card">
                 <div class="inner-card-front">
                     <p>
-                        ${questionText}
+                        ${data.question}
                     </p>
                 </div>
                 <div class="inner-card-back">
                     <p>
-                        ${answerText}
+                        ${data.answer}
                     </p>
                 </div>
             </div>
         `
-    console.log(card);
+    cardsEl.push(card)
+
+    card.addEventListener('click', ()=> {
+        card.classList.toggle('show-answer')
+    })
+
     cardsContainer.appendChild(card)
     addContainer.classList.remove('show')
-    question.value = '';
-    answer.value = '';
-        
-    }
+
+    updateCurrentText();
 }
 
-
-
-
 prevBtn.addEventListener('click', () => {
-    let previousCard = document.querySelector('.card')
-    previousCard.classList.remove('active')
-    previousCard.classList.add('left')
-    console.log(previousCard)
+
+    cardsEl[currentCard].className = 'card right'
+
+    currentCard = currentCard - 1;
+
+    if (currentCard < 0) {
+        currentCard = 0;
+    }
+
+    cardsEl[currentCard].className = 'card active'
+
+    updateCurrentText();
+    console.log('next')
 })
 
 nextBtn.addEventListener('click', () => {
+ 
+    cardsEl[currentCard].className = 'card left'
+    
+    currentCard = currentCard + 1;
+
+    if (currentCard > cardsEl.length - 1) {
+        currentCard = cardsEl.length - 1;
+    }
+    cardsEl[currentCard].className = 'card active'
+ 
+    updateCurrentText();
     console.log('next')
 })
 
@@ -64,8 +140,10 @@ hideBtn.addEventListener('click', () => {
     addContainer.classList.remove('show')
 })
 
-addCard.addEventListener('click', createCard)
+addCard.addEventListener('click', createNewCard)
 
 clear.addEventListener('click', () => {
     cardsContainer.innerHTML = '';
 })
+
+createCards();
