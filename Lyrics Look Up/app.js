f2f12e02c533cea0540eed9a545ebe380b1ee949
TheJackForge <kimfidler@gmail.com>
2020-11-25 12:19:02 -0700

    Lyrics Project - Custom Code

--- Lyrics Look Up/app.js
+++ Lyrics Look Up/app.js
@@ -4,6 +4,7 @@ const resultContainer = document.getElementById('result')
 const moreResults = document.getElementById('more');
 
 const apiUrl = 'https://api.lyrics.ovh'
+let songTemp;
 
 // Functions
 
@@ -13,6 +14,19 @@ async function getSongInfo(term) {
     .then (data => populateDOM(data.data))
 }
 
+async function getSongInfoNext(term) {
+    fetch(`${apiUrl}/suggest/search?limit=15&q=${term}&index=15`)
+    .then (response => response.json())
+    .then (data => populateDOM(data.data))
+}
+
+async function getSongInfoPrev(term) {
+    fetch(`${apiUrl}/suggest/search?limit=15&q=${term}&index=15`)
+    .then (response => response.json())
+    .then (data => populateDOM(data.data))
+}
+
+
 function populateDOM(results) {
     const list = document.createElement('ul');
     list.classList.add('songs')
@@ -20,16 +34,19 @@ function populateDOM(results) {
     const searchResults = results
     searchResults.forEach(single => {
         const songItem = document.createElement('li')
+        const id = single.id
         const artist = single.artist.name
         const title =  single.title
         songItem.innerHTML = `
         <span><strong>${artist}</strong> - ${title}</span>
-        <button class="btn">Get Lyrics</button>
+        <button class="btn" id="${id}">Get Lyrics</button>
         `
         list.appendChild(songItem)
 
         moreResults.innerHTML = `
-        <button class="btn">Previous</button> <button class="btn">Next</button>
+
+
+        <button class="btn" id="prev">Previous</button> <button class="btn" id="next">Next</button>
         `
     })
     
@@ -40,12 +57,36 @@ function populateDOM(results) {
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    moreResults.innerHTML = '';
     resultContainer.innerHTML = '';
     const searchValue = search.value
     
     if (searchValue !== '') {
+        songTemp = searchValue;
         getSongInfo(searchValue)
     } else {
         alert('Please fill in all fields')
+    }
+})
+
+moreResults.addEventListener('click', (e) => {
+    if (e.target.id === 'next') {
+        moreResults.innerHTML = ''
+        resultContainer.innerHTML = ''
+        getSongInfoNext(songTemp)
+    }
+})
+
+moreResults.addEventListener('click', (e) => {
+    if (e.target.id === 'prev') {
+        moreResults.innerHTML = ''
+        resultContainer.innerHTML = ''
+        getSongInfoPrev(songTemp)
+    }
+})
+
+resultContainer.addEventListener('click', (e) => {
+    if (e.target.classList.contains('btn')) {
+        console.log(e.target.id)
     }
 })
