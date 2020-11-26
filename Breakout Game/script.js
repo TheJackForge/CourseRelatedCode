9ab20ce2e4091e52f5ce1163c134b69153914593
TheJackForge <kimfidler@gmail.com>
2020-11-26 12:26:32 -0700

    Started on Breakout Game

---
+++ Breakout Game/script.js
@@ -0,0 +1,11 @@
+const rules = document.getElementById('rules');
+const rulesBtn = document.getElementById('rules-btn');
+const closeBtn = document.getElementById('close-btn')
+
+rulesBtn.addEventListener('click', () => {
+    rules.classList.add('show');
+})
+
+closeBtn.addEventListener('click', () => {
+    rules.classList.remove('show')
+})
