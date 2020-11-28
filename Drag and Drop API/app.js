c1cf9aae3786153565e0cfd4af3f1badc8ef9c4c
TheJackForge <kimfidler@gmail.com>
2020-11-27 21:08:09 -0700

    Drag and Drop Project Complete

--- Drag and Drop API/app.js
+++ Drag and Drop API/app.js
@@ -45,5 +45,83 @@ function createList() {
         listItems.push(listItem)
 
         draggable_list.appendChild(listItem)
+    });
+
+    addEventListeners();
+}
+
+function dragStart() {
+    // console.log('Event: ', 'dragstart');
+    dragStartIndex = +this.closest('li').getAttribute('data-index');
+
+    
+}
+
+// Swap list items that are drag and drop
+
+function swapItems(fromIndex, toIndex) {
+    console.log(fromIndex)
+    console.log(toIndex)
+    const itemOne = listItems[fromIndex].querySelector('.draggable')
+    const itemTwo = listItems[toIndex].querySelector('.draggable')
+
+    listItems[fromIndex].appendChild(itemTwo);
+    listItems[toIndex].appendChild(itemOne);
+}
+
+function dragEnter() {
+    // console.log('Event: ', 'dragenter');
+    this.classList.add('over')
+}
+
+function dragOver(e) {
+    // console.log('Event: ', 'dragover');
+    e.preventDefault();
+}
+
+function dragLeave() {
+    // console.log('Event: ', 'dragleave');
+    this.classList.remove('over')
+}
+
+function dragDrop() {
+    // console.log('Event: ', 'drop');
+    const dragEndIndex = +this.getAttribute('data-index');
+
+    swapItems(dragStartIndex, dragEndIndex);
+
+    this.classList.remove('over')
+}
+
+// Check the order of list items
+
+function checkOrder() {
+    listItems.forEach((listItem, index) => {
+        const websiteName = listItem.querySelector('.draggable').innerText.trim();
+
+        if (websiteName !== websiteTraffic[index]) {
+            listItem.classList.add('wrong');
+        } else {
+            listItem.classList.remove('wrong');
+            listItem.classList.add('right');
+        }
     })
 }
+
+function addEventListeners() {
+    const draggables = document.querySelectorAll('.draggable')
+    const dragListItems = document.querySelectorAll('.draggable-list li')
+
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', dragStart)
+    })
+
+    dragListItems.forEach(item => {
+        item.addEventListener('dragover', dragOver)
+        item.addEventListener('drop', dragDrop)
+        item.addEventListener('dragenter', dragEnter)
+        item.addEventListener('dragleave', dragLeave)
+    })
+}
+
+check.addEventListener('click', checkOrder)
