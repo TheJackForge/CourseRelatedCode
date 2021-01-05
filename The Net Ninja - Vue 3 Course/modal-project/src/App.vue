74212f390e649762ea53246c918c923091c54380
thejackforge <kimfidler@gmail.com>
2021-01-05 12:59:23 -0700

    Course Progress

---
+++ The Net Ninja - Vue 3 Course/modal-project/src/App.vue
@@ -0,0 +1,95 @@
+<template>
+  <h1>{{title}}</h1>
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up now</a>
+        <a href="#">More info</a>
+      </template>
+      <h1>Ninja Giveaway!</h1>
+      <p>Grab your ninja swag for half price!</p>
+    </Modal>
+  </div>
+  <div v-if="showModal2">
+    <Modal theme="extremeSale" @close"toggleModalTwo">
+      <template v-slot:links2>
+        <span>YOU HAVE JUST UNLOCKED AMAZING SAVINGS!</span>
+      </template>
+      <h1>Ninja Giveaway!</h1>
+      <p>Grab your ninja swag for half price!</p>
+    </Modal>
+  </div>
+  <button @click.alt="toggleModal">open modal (alt)</button>
+  <button @click="toggleModalTwo">open modal 2</button>
+</template>
+
+<template>
+  <h1>{{title}}</h1>
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up now</a>
+        <a href="#">More info</a>
+      </template>
+      <h1>Ninja Giveaway!</h1>
+      <p>Grab your ninja swag for half price!</p>
+    </Modal>
+  </div>
+  <div v-if="showModal2">
+    <Modal theme="extremeSale" @close"toggleModalTwo">
+      <template v-slot:links2>
+        <span>YOU HAVE JUST UNLOCKED AMAZING SAVINGS!</span>
+      </template>
+      <h1>Ninja Giveaway!</h1>
+      <p>Grab your ninja swag for half price!</p>
+    </Modal>
+  </div>
+  <button @click.alt="toggleModal">open modal (alt)</button>
+  <button @click="toggleModalTwo">open modal 2</button>
+</template>
+
+<script>
+
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: {Modal},
+  data() {
+    return {
+      title: 'My First Vue App',
+      header: 'Sign up for the Giveaway!',
+      text: 'Grab your ninja swag for half price!',
+      showModal: false,
+      showModal2: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModal2 = !this.showModal2
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
