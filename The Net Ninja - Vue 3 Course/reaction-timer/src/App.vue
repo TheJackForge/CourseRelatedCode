3c75af6b1718e88c74028e2ae2792addfb6fe68e
thejackforge <kimfidler@gmail.com>
2021-01-06 10:45:17 -0700

    Reflexes Project Complete

--- The Net Ninja - Vue 3 Course/reaction-timer/src/App.vue
+++ The Net Ninja - Vue 3 Course/reaction-timer/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">PLAY</button>
+  <Results v-if="showResults" :score="score"/>
   <Block v-if="isPlaying" :delay="delay" @end="endGame" />
-  <p v-if="showResults">Reaction time: {{ score }} ms</p>
+  
 </template>
 
 <script>
 import Block from './components/Block'
+import Results from './components/Results'
 
 export default {
   name: 'App',
-  components: { Block },
+  components: { Block, Results }, 
   data() {
     return {
       isPlaying: false,
@@ -24,13 +26,11 @@ export default {
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
       this.showResults = false
-      console.log(this.isPlaying)
     },
     endGame(reactionTime) {
       this.score = reactionTime
       this.isPlaying = false
       this.showResults = true
-      console.log(this.isPlaying)
     }
   }
 }
@@ -45,4 +45,20 @@ export default {
   color: #444;
   margin-top: 60px;
 }
+
+    button {
+        background: #0faf87;
+        color: #fff;
+        border: none;
+        padding: 8px 16px;
+        letter-spacing: 1px;
+        cursor: pointer;
+        margin: 10px;
+        border-radius: 5px;
+    }
+
+    button[disabled] {
+        opacity: 0.2;
+        cursor: not-allowed;
+    }
 </style>
