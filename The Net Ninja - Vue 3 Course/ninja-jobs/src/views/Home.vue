22f62a8c7442295f2438c38c686b10dcf94de977
thejackforge <kimfidler@gmail.com>
2021-01-07 22:46:07 -0700

    Redirects in Vue

---
+++ The Net Ninja - Vue 3 Course/ninja-jobs/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil fuga commodi in et numquam veritatis! Accusamus perspiciatis recusandae, dicta amet soluta ab? Vero sapiente error expedita perspiciatis pariatur impedit ad corporis aliquam repellat ipsum sit, sequi assumenda culpa iusto dicta modi in excepturi recusandae magnam beatae! Doloremque ex, atque nulla, ea vero nemo delectus, quo quas distinctio illum culpa deserunt nesciunt provident ipsum? Obcaecati minus dicta, rem commodi provident maxime aliquam voluptatum repellat laborum quam eius inventore, possimus ipsa veniam pariatur recusandae voluptatibus ea reiciendis incidunt quia! Sint voluptatum tempore ratione praesentium, facere, qui laudantium cupiditate exercitationem, consectetur veritatis laboriosam?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil fuga commodi in et numquam veritatis! Accusamus perspiciatis recusandae, dicta amet soluta ab? Vero sapiente error expedita perspiciatis pariatur impedit ad corporis aliquam repellat ipsum sit, sequi assumenda culpa iusto dicta modi in excepturi recusandae magnam beatae! Doloremque ex, atque nulla, ea vero nemo delectus, quo quas distinctio illum culpa deserunt nesciunt provident ipsum? Obcaecati minus dicta, rem commodi provident maxime aliquam voluptatum repellat laborum quam eius inventore, possimus ipsa veniam pariatur recusandae voluptatibus ea reiciendis incidunt quia! Sint voluptatum tempore ratione praesentium, facere, qui laudantium cupiditate exercitationem, consectetur veritatis laboriosam?</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: { }
+}
+</script>
