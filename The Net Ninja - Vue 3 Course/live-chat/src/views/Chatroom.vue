75076df98d4c6e8da9c24c1abf30908c1a91224e
thejackforge <kimfidler@gmail.com>
2021-01-15 11:45:10 -0700

    Chatroom routing authentication

--- The Net Ninja - Vue 3 Course/live-chat/src/views/Chatroom.vue
+++ The Net Ninja - Vue 3 Course/live-chat/src/views/Chatroom.vue
@@ -1,13 +1,38 @@
 <template>
-  <p>Chatroom</p>
+  <div class="container">
+    <Navbar />
+  </div>
 </template>
 
 <script>
+import Navbar from '../components/Navbar'
+
 export default {
+  components: { Navbar },
+  setup() {
+    
+  }
 
 }
 </script>
 
 <style>
+  nav {
+    padding: 20px;
+    border-bottom: 1px solid #eee;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 
+  nav p {
+    margin: 2px auto;
+    font-size: 16px;
+    color: #444;
+  }
+
+  nav p.email {
+    font-size: 14px;
+    color: #999;
+  }
 </style>
